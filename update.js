c7bc059f1d8155c2d6420f8ab7edd9b0d3d44c9a
Richard P. Gonzales <richard@paperhousebrewing.com>
2020-01-11 14:57:53 -0600

    Changed formatting and added error output.

--- update.js
+++ update.js
@@ -3,14 +3,15 @@ require('tingodb');
 module.exports = function (RED) {
     function TingoDBUpdateNode(config) {
         RED.nodes.createNode(this, config);
-
         var node = this;
+
         node.multi = config.multi;
         node.upsert = config.upsert;
+
         node.on('input', function (msg) {
             var db = RED.nodes.getNode(config.db).getDB();
             db.collection(config.collection).update(msg.predicate, msg.update, { multi: node.multi, upsert:node.upsert}, function (err) {
-                node.send(msg);
+                node.send(err || msg);
             });
         });
     }
